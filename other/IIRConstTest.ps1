5cad48fab8045f7c3467124f6878f687ccd93e5a
CaiB <cai.biesinger@outlook.com>
2021-03-01 14:28:23 -0800

    Made script to help test some IIR math

---
+++ other/IIRConstTest.ps1
@@ -0,0 +1,17 @@
+[double] $IIRConst = 0.9791483623;
+[int] $SampleRate = 300;
+[double] $TargetValue = 0.9999;
+
+Write-Host "Using IIR const $IIRConst and sample rate $SampleRate Hz";
+
+[double] $NewVal = 1.0;
+[double] $OutputVal = 0.0;
+[int] $CyclesTaken = 0;
+while($OutputVal -LE $TargetValue)
+{
+    $OutputVal = ($IIRConst * $OutputVal) + ((1.0 - $IIRConst) * $NewVal);
+    $CyclesTaken++;
+}
+
+$TimeTaken = $CyclesTaken * 1000.0 / $SampleRate;
+Write-Host "It took $CyclesTaken samples ($TimeTaken ms) to get to the target value.";
