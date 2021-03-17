faed6b139900c3d8db54e27cad8da33029e8bc19
CaiB <cai.biesinger@outlook.com>
2021-03-16 21:52:32 -0700

    Fixing syntax errors and removing old debug signals

--- src/LEDCountCalc.sv
+++ src/LEDCountCalc.sv
@@ -7,12 +7,12 @@ module LEDCountCalc #(
     parameter W = 6,                        // number of whole bits in the fixed point format
     parameter D = 10,                       // number of decimal  bits in the fixed point format - precision to ~.001
     parameter LEDS = 50,                    // total which the individual counts should sum to
-    parameter BIN_QTY  = 12                 // number of independant notes being processed
+    parameter BIN_QTY  = 12,                 // number of independant notes being processed
 
     // =============== Fixed Point Specific Parameters ===============
     // The following parameters are computed based on the above parameters W and D. 
     // See LinearVisualizer.sv : line 24 for instruction on how to recompute
-    parameter LEDS_X = 20,                  // 0.0195... ~ 20 ~ 0000010100 - D bit fixed point inverse of LEDS
+    parameter LEDS_X = 20                  // 0.0195... ~ 20 ~ 0000010100 - D bit fixed point inverse of LEDS
 ) (
     output logic [BIN_QTY - 1 : 0][$clog2(LEDS) - 1 : 0] LEDCount,  // integer ratios of the amplitudes which should add up to LEDS
     output logic data_v,                                            // set high when the output data is valid
